8769509a8fd96cb529f1853a84d4808809ed1840
lepsic <Lepsick2003@yandex.ru>
2023-04-19 03:08:33 +0300

    added task8

---
+++ Base/task_08.py
@@ -0,0 +1,20 @@
+def multiply_numbers(inputs=None):
+    output_value = 1
+    if inputs is None:
+        return None
+    check_value = False
+    inputs = str(inputs)
+    for elements in inputs:
+        if type(elements) == str:
+            try:
+                elements = int(elements)
+                check_value = True
+                output_value *= elements
+            except ValueError:
+                continue
+    if not check_value:
+        return None
+    return output_value
+
+
+print(multiply_numbers([5,6,4]))
