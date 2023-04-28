d442be9f26422c93b22f20094c645430af88a0de
lepsic <Lepsick2003@yandex.ru>
2023-04-28 18:02:29 +0300

    fix returns

--- task_01.py
+++ task_01.py
@@ -6,7 +6,7 @@ alphavit = set("1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
 
 def is_palindrome(input_data):
     if input_data is None:
-        return None
+        return False
     input_data = str(input_data)
     input_data = ''.join(filter(alphavit.__contains__, input_data)).lower()
     revere_str = input_data[::-1]
