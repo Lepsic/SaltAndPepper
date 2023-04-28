f76a14cb82fa875e3116a04cdd7c8e777bd2201d
lepsic <Lepsick2003@yandex.ru>
2023-04-28 18:03:13 +0300

    fix returns format

--- task_05.py
+++ task_05.py
@@ -3,9 +3,9 @@ import datetime
 
 def date_in_future(days) -> datetime:
     if type(days) != int:
-        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     current_date = datetime.datetime.now()
     day = datetime.timedelta(days=days)
     current_date = current_date + day
-    return current_date.strftime("%Y-%m-%d %H:%M:%S")
+    return current_date.strftime("%d-%m-%Y %H:%M:%S")
 
